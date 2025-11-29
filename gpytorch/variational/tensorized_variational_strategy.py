6069015cb00c3ff0dc47ab58a30c6a5974fb9ef1
Kaiwen Wu <kwwu2015@gmail.com>
2025-11-28 22:52:47 -0500

    use vs algebra

--- gpytorch/variational/tensorized_variational_strategy.py
+++ gpytorch/variational/tensorized_variational_strategy.py
@@ -6,11 +6,9 @@ import torch
 
 from linear_operator import to_dense
 
-from linear_operator.operators import CholLinearOperator, LinearOperator, MatmulLinearOperator, SumLinearOperator
+from linear_operator.operators import DiagLinearOperator, LinearOperator, SumLinearOperator
 from linear_operator.utils.cholesky import psd_safe_cholesky
 from torch import Tensor
-
-from gpytorch.settings import trace_mode
 
 from ..distributions import MultivariateNormal
 from .variational_strategy import VariationalStrategy
@@ -97,37 +95,23 @@ class TensorizedVariationalStrategy(VariationalStrategy):
         induc_data_covar = full_covar[..., :num_induc, num_induc:].to_dense()
         data_data_covar = full_covar[..., num_induc:, num_induc:]
 
-        # Compute interpolation terms
-        # K_ZZ^{-1/2} K_ZX
-        # K_ZZ^{-1/2} \mu_Z
         L = psd_safe_cholesky(to_dense(induc_induc_covar))
         if L.shape != induc_induc_covar.shape:
             raise NotImplementedError()
 
-        interp_term = torch.linalg.solve_triangular(
-            L.to(full_inputs.dtype),
-            # induc_data_covar.type(_linalg_dtype_cholesky.value()),
+        predictive_mean, predictive_covar_diag = VariationalStrategyAlgebra.apply(
+            L,
             induc_data_covar,
-            upper=False,
-        ).to(full_inputs.dtype)
+            to_dense(variational_inducing_covar) - to_dense(self.variational_distribution.covariance_matrix),
+            inducing_values,
+        )
 
-        # Compute the mean of q(f)
-        # k_XZ K_ZZ^{-1/2} (m - K_ZZ^{-1/2} \mu_Z) + \mu_X
-        predictive_mean = (interp_term.transpose(-1, -2) @ inducing_values.unsqueeze(-1)).squeeze(-1) + test_mean
+        predictive_mean = predictive_mean + test_mean
 
-        # Compute the covariance of q(f)
-        # K_XX + k_XZ K_ZZ^{-1/2} (S - I) K_ZZ^{-1/2} k_ZX
-        assert isinstance(variational_inducing_covar, CholLinearOperator)
-        root_times_interp = variational_inducing_covar.root.transpose(-1, -2) @ interp_term
-
-        if trace_mode.on():
-            raise NotImplementedError
-        else:
-            predictive_covar = SumLinearOperator(
+        return MultivariateNormal(
+            predictive_mean,
+            SumLinearOperator(
                 data_data_covar.add_jitter(self.jitter_val),
-                MatmulLinearOperator(interp_term.transpose(-1, -2), interp_term).mul(-1),
-                MatmulLinearOperator(root_times_interp.transpose(-1, -2), root_times_interp),
-            )
-
-        # Return the distribution
-        return MultivariateNormal(predictive_mean, predictive_covar)
+                DiagLinearOperator(predictive_covar_diag),
+            ),
+        )
